851140c975af9e9afba27357cb521f3dcc78f938
zuma990 <zumajuve@gmail.com>
2022-04-05 10:44:09 +0200

    Added new file

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below line
+function myTrueBoolean(){
+    return true;
+}
+myTrueBoolean();
+console.log(myTrueBoolean());
+// Only change code above line
+module.exports = myTrueBoolean;
