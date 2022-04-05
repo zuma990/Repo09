351670fac35c3a088abe959bf71b2177e5ab2438
zuma990 <zumajuve@gmail.com>
2022-04-05 13:46:38 +0200

    Added new file

---
+++ ex10/src/index.js
@@ -0,0 +1,27 @@
+// Only change code below this line
+function mathTest(x, y){
+
+    if(x<0 || y<0){
+        return undefined;
+    } 
+    return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
+
+
+}
+mathTest(2, 2);
+mathTest(-2, 2);
+mathTest(2, -2);
+mathTest(2, 8);
+mathTest(3, 3);
+mathTest(0, 0);
+
+console.log(mathTest(2, 2));
+console.log(mathTest(-2, 2));
+console.log(mathTest(2, -2));
+console.log(mathTest(2, 8));
+console.log(mathTest(3, 3));
+console.log(mathTest(0, 0));
+
+// Only change code above this line
+
+module.exports = mathTest;
