eb614db03ff1e2470a5e038197963648dadadc88
zuma990 <zumajuve@gmail.com>
2022-04-05 14:02:03 +0200

    Added new file

---
+++ ex12/src/index.js
@@ -0,0 +1,25 @@
+// Only change code below this line
+function logicOrder(num){
+
+    if(num < 50){
+        return "Less than 50";
+    }
+    else if(num < 100){
+        return "Less than 100";
+    }
+    else {
+        return "Greater than or equal to 100";
+    }
+    
+}
+
+logicOrder(28);
+logicOrder(60);
+logicOrder(110);
+
+console.log(logicOrder(28));
+console.log(logicOrder(60));
+console.log(logicOrder(110));
+// Only change code above this line
+
+module.exports =logicOrder;
