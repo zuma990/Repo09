25bdc72ebd68e8f7cc94a097c440e730105a5fba
zuma990 <zumajuve@gmail.com>
2022-04-05 11:30:52 +0200

    Added new file

---
+++ ex05/src/index.js
@@ -0,0 +1,21 @@
+// Only change code below this line
+function compareStrictValues(m){
+    if(m != 55){
+        return "Not equal";
+    }
+    return "Equal";
+    
+}
+compareStrictValues(55);
+compareStrictValues("55");
+compareStrictValues("21");
+compareStrictValues(12);
+compareStrictValues("Doe");
+
+console.log(compareStrictValues(55));
+console.log(compareStrictValues("55"));
+console.log(compareStrictValues("21"));
+console.log(compareStrictValues(12));
+console.log(compareStrictValues("Doe"));
+// Only change code above this line
+module.exports = compareStrictValues;
