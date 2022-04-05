f5f800546da03375c1c4074d6ea3e8da9b8bf25c
zuma990 <zumajuve@gmail.com>
2022-04-05 15:56:59 +0200

    Added new file

---
+++ ex17/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function inverseWhile(){
+    var fiveNumbers = 5;
+    while( fiveNumbers >0){
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+return fiveNumbers;
+}
+// Only change code above this line
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
