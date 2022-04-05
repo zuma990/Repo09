42ed5a992beb0683dbdc1f125c7dc1df5083b300
zuma990 <zumajuve@gmail.com>
2022-04-05 14:06:44 +0200

    Added new file

---
+++ ex13/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function isLess(a, b) {
+
+    if(a < b){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+isLess(10, 15);
+isLess(15, 10);
+
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+// Only change code above this line
+
+module.exports = isLess;
